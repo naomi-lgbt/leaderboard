63ddc0abda1d4cd817113c124feac3f03f710591
Naomi Carrigan <nhcarrigan@gmail.com>
2023-06-29 15:27:58 -0700

    feat: test the new components

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      declarations: [AppComponent, NavComponent],
     })
   );
 
--- client/src/app/logo/logo.component.spec.ts
+++ client/src/app/logo/logo.component.spec.ts
@@ -5,6 +5,7 @@ import { LogoComponent } from './logo.component';
 describe('LogoComponent', () => {
   let component: LogoComponent;
   let fixture: ComponentFixture<LogoComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +14,23 @@ describe('LogoComponent', () => {
     fixture = TestBed.createComponent(LogoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.width).toBeDefined();
+  });
+
+  it('should default to a width of 500', () => {
+    const logo = compiled.querySelector('svg');
+    expect(logo?.getAttribute('width')).toBe('500');
+  });
+
+  it('should accept a custom width', () => {
+    component.width = 100;
+    fixture.detectChanges();
+    const logo = compiled.querySelector('svg');
+    expect(logo?.getAttribute('width')).toBe('100');
   });
 });
